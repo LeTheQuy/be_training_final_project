c56dd7a6144fc2dbee329c145009df9e290a71b3
QUY LE THE <quy@gotitapp.co>
2021-06-18 15:31:28 +0700

    feat: update to use pw hash

--- main/controllers/auth.py
+++ main/controllers/auth.py
@@ -1,7 +1,8 @@
-from flask import request, jsonify, Blueprint
+from flask import request, jsonify
 from flask_jwt_extended import create_access_token
 
 from main.app import app
+from main.helpers.pw import generate_password_hash, verify_password_with_password_hash
 from main.models.user import User
 
 
@@ -11,7 +12,8 @@ def register_user():
     if User.find_by_username(request_data["username"]):
         return jsonify({"message": "Duplicated username"}), 400
     else:
-        user = User(**request_data)
+        password_hash = generate_password_hash(request_data["password"])
+        user = User(username=request_data["username"], password_hash=password_hash)
         user.save_to_db()
         return jsonify({"message": "User created successfully"}), 201
 
@@ -20,7 +22,7 @@ def register_user():
 def sign_in():
     request_data = request.get_json()
     user = User.find_by_username(request_data["username"])
-    if user and user.password == request_data["password"]:
+    if user and verify_password_with_password_hash(request_data["password"], user.password_hash):
         access_token = create_access_token(identity=user.id, fresh=True)
         return {"access_token": access_token}, 200
     else:
