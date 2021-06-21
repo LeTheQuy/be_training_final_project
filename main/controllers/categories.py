ad798e5d5153d4402e2329d527fc67e586f64842
QUY LE THE <quy@gotitapp.co>
2021-06-22 01:41:59 +0700

    refactor: update to use load_request_data_by_schema

--- main/controllers/categories.py
+++ main/controllers/categories.py
@@ -1,10 +1,8 @@
-from flask import jsonify
-from marshmallow import ValidationError
-
 from main.app import app
 from main.helpers.auth import jwt_required
 from main.models.category import Category
 from main.schemas.category_schema import CategorySchema
+from main.schemas.validation import load_request_data_by_schema
 
 category_schema = CategorySchema()
 categories_schema = CategorySchema(many=True, only=("id", "name"))
@@ -24,23 +22,19 @@ def get_category_by_id(_id):
         data = category_schema.dump(category)
     else:
         data = {"message": "invalid category id"}
-    return jsonify(data)
+    return data
 
 
 @app.route("/categories/<string:name>", methods=["POST"])
 @jwt_required()
+@load_request_data_by_schema(category_schema)
 def add_category(name):
-    try:
-        category_schema.load({"name": name})
-    except ValidationError as err:
-        return jsonify({"message": err.messages})
-
     category = Category.find_by_name(name)
     if category:
-        return jsonify({"message": "Duplicated category name"}), 400
+        return {"message": "Duplicated category name"}, 400
     else:
         category = Category(name=name)
         category.save_to_db()
         result = category_schema.dump(category)
         result["message"] = "Category added"
-        return jsonify(result)
+        return result
