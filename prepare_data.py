05220aad711afd162cd280318769c2d06f2580b6
QUY LE THE <quy@gotitapp.co>
2021-06-22 00:00:48 +0700

    feat: reformat

--- prepare_data.py
+++ prepare_data.py
@@ -15,8 +15,8 @@ CATEGORIES = ["FOOTBALL", "TENNIS", "VOLLEYBALL", "SWING", "MARATHON", "JOGGING"
 for category in CATEGORIES:
     Category(category).save_to_db()
     for i in range(10):
-        item = Item(f"{category} Item {i*1}", f"Item description for category {category}, item {i}: A ball is a round object with "
-                                 f"various uses. It is used in ball games, where the play of the game follows the "
-                                 f"state of the ball as it is hit, kicked or thrown by players. ",
-                    root_user.id, Category.find_all()[0].id)
+        item = Item(f"{category} Item {i * 1}",
+                    f"Item description for category {category}, item {i}: A ball is a round object with various uses. "
+                    f"It is used in ball games, where the play of the game follows the state of the ball as it is hit, "
+                    f"kicked or thrown by players. ", root_user.id, Category.find_all()[0].id)
         item.save_to_db()
