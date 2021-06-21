d9447a3e3d59edcd938c605e387e0a059843e4b2
QUY LE THE <quy@gotitapp.co>
2021-06-21 23:59:33 +0700

    feat: update dummy data for test

--- prepare_data.py
+++ prepare_data.py
@@ -10,10 +10,13 @@ db.create_all()
 root_user = User("root", generate_password_hash("123456"))
 root_user.save_to_db()
 
-CATEGORIES = ["FOODBALL", "TENNIS", "VOLLEYBALL", "SWING", "MARATHON", "JOGGING"]
+CATEGORIES = ["FOOTBALL", "TENNIS", "VOLLEYBALL", "SWING", "MARATHON", "JOGGING"]
 
 for category in CATEGORIES:
     Category(category).save_to_db()
-
-item = Item("First Item", "Fist Item description ..... ", root_user.id, Category.find_all()[0].id)
-item.save_to_db()
+    for i in range(10):
+        item = Item(f"{category} Item {i*1}", f"Item description for category {category}, item {i}: A ball is a round object with "
+                                 f"various uses. It is used in ball games, where the play of the game follows the "
+                                 f"state of the ball as it is hit, kicked or thrown by players. ",
+                    root_user.id, Category.find_all()[0].id)
+        item.save_to_db()
