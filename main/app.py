6325fa2a605c9169ead1f9951285e92ae7de7bef
QUY LE THE <quy@gotitapp.co>
2021-06-21 23:48:11 +0700

    refactor: remove unnecessary code

--- main/app.py
+++ main/app.py
@@ -3,14 +3,12 @@ from flask import Flask
 from main.config.config import config
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = config.get_sqlalchemy_db_uri()
+app.config[
+    "SQLALCHEMY_DATABASE_URI"] = f"mysql+pymysql://{config.MYSQL_USERNAME}:{config.MYSQL_PASSWORD}" \
+                                 f"@{config.MYSQL_HOST}:{config.MYSQL_PORT}/{config.MYSQL_DB}"
+
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = config.SECRET_KEY
-
-
-@app.route("/")
-def hello():
-    return {"hello": "world"}
 
 
 def _register_modules():
