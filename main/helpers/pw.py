0419fc076688b8f159a601611b6794dd13623a38
QUY LE THE <quy@gotitapp.co>
2021-06-21 17:50:46 +0700

    feat: add encode

--- main/helpers/pw.py
+++ main/helpers/pw.py
@@ -3,8 +3,8 @@ import bcrypt
 
 def generate_password_hash(password):
     salt = bcrypt.gensalt()
-    return bcrypt.hashpw(password, salt)
+    return bcrypt.hashpw(password.encode('utf-8'), salt)
 
 
 def verify_password_with_password_hash(password, password_hash):
-    return bcrypt.checkpw(password, password_hash)
+    return bcrypt.checkpw(password.encode('utf-8'), password_hash.encode('utf-8'))
