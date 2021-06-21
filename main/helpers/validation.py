4cb82cb2552aa27f0e67c17a097a11ca78f9ccc5
QUY LE THE <quy@gotitapp.co>
2021-06-21 15:37:40 +0700

    feat: add exist_category method

--- main/helpers/validation.py
+++ main/helpers/validation.py
@@ -1,6 +1,13 @@
 from marshmallow import ValidationError
 
+from main.models.category import Category
+
 
 def must_not_be_blank(data):
     if not data:
         raise ValidationError("Data not provided.")
+
+
+def exist_category(category_id):
+    if Category.find_by_id(category_id) is None:
+        raise ValidationError("Invalid category id")
