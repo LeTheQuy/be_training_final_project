14f87d21620d304c743d00b0306d2c6b775ae21d
QUY LE THE <quy@gotitapp.co>
2021-06-22 13:05:34 +0700

    fix: merge post_load

--- main/schemas/category_item_schema.py
+++ main/schemas/category_item_schema.py
@@ -9,17 +9,13 @@ class CategoryItemSchema(Schema):
     item_id = fields.Integer(required=True, validate=[exist_item])
     user_id = fields.Integer(required=True)
 
-    @post_load
-    def process_input(self, data, **kwargs):
+    @post_load(pass_many=True)
+    def wrap(self, data, many, **kwargs):
         item = Item.find_by_id(data["item_id"])
         if item.category_id != data["category_id"]:
             raise ValidationError("Category and item don't match")
         if data["user_id"] is not None and item.user_id != data["user_id"]:
             raise ValidationError("This item is not editable by yourself")
-        return data
-
-    @post_load(pass_many=True)
-    def wrap(self, data, many, **kwargs):
         return Item.find_by_id(data["item_id"])
 
 
