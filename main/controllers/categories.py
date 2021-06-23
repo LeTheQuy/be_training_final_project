0d039ce12bbb6e8fa70431016cafabe44e396c99
QUY LE THE <quy@gotitapp.co>
2021-06-23 16:02:38 +0700

    refactor: update per FB

--- main/controllers/categories.py
+++ main/controllers/categories.py
@@ -1,39 +1,18 @@
 from main.app import app
-from main.helpers.auth import jwt_required
+from main.helpers.parser import parse_data
 from main.models.category import Category
-from main.schemas.category_schema import CategorySchema
-from main.schemas.validation import load_request_data_by_schema
-
-category_schema = CategorySchema()
-categories_schema = CategorySchema(many=True, only=("id", "name"))
+from main.schemas.base.category import CategorySchema
 
 
 @app.route("/categories", methods=["GET"])
 def get_categories():
     categories = Category.find_all()
-    result = categories_schema.dump(categories)
+    result = CategorySchema(many=True, only=("id", "name")).dump(categories)
     return result
 
 
-@app.route("/categories/<int:_id>", methods=["GET"])
-def get_category_by_id(_id):
-    category = Category.find_by_id(_id)
-    if category:
-        data = category_schema.dump(category)
-    else:
-        data = {"message": "invalid category id"}, 400
+@app.route("/categories/<int:category_id>", methods=["GET"])
+@parse_data()
+def get_category_by_id(category_id, category):
+    data = CategorySchema().dump(category)
     return data
-
-
-@app.route("/categories/<string:name>", methods=["POST"])
-@jwt_required()
-def add_category(name, user_id):
-    category = Category.find_by_name(name)
-    if category:
-        return {"message": "Duplicated category name"}, 400
-    else:
-        category = Category(name=name)
-        category.save_to_db()
-        result = category_schema.dump(category)
-        result["message"] = "Category added"
-        return result
