be157893ea5aa52ee68312cc38ba0619b7e56b31
QUY LE THE <quy@gotitapp.co>
2021-06-23 10:24:03 +0700

    fix: update config module per FB

--- main/config/base.py
+++ main/config/base.py
@@ -1,9 +1,9 @@
-class _Config(object):
+class BaseConfig:
     MYSQL_HOST = None
     MYSQL_PORT = None
     MYSQL_USERNAME = None
     MYSQL_PASSWORD = None
     MYSQL_DB = None
     DEBUG = False
-    ITEM_PER_PAGE = None
+    ITEMS_PER_PAGE = None
 
--- main/config/local.py
+++ main/config/local.py
@@ -1,7 +1,7 @@
-from main.config.base import _Config
+from main.config.base import BaseConfig
 
 
-class _LocalConfig(_Config):
+class LocalConfig(BaseConfig):
     MYSQL_HOST = "127.0.0.1"
     MYSQL_PORT = 3306
     MYSQL_USERNAME = "root"
@@ -9,7 +9,7 @@ class _LocalConfig(_Config):
     MYSQL_DB = "be_training"
     DEBUG = True
     SECRET_KEY = "be_training_g0tit@^&#quydz__tqn_"
-    ITEM_PER_PAGE = 10
+    ITEMS_PER_PAGE = 10
 
 
-config = _LocalConfig
+config = LocalConfig
