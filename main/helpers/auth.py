dedae22ec8f0e8a496d1c2381117c29071242368
QUY LE THE <quy@gotitapp.co>
2021-06-23 11:06:19 +0700

    refactor: add exception when handle encode, decode jwt

--- main/helpers/auth.py
+++ main/helpers/auth.py
@@ -1,38 +1,48 @@
 import functools
 
 import jwt
-from flask import request
+from flask import request, abort
 
 from main.config.config import config
 
 
 def encode_identity(identity):
-    encoded_jwt = jwt.encode({"identity": identity}, config.SECRET_KEY, algorithm="HS256")
-    return encoded_jwt
+    try:
+        encoded_jwt = jwt.encode({"identity": identity}, config.SECRET_KEY, algorithm="HS256")
+        return encoded_jwt
+    except Exception as e:
+        abort(400, description=str(e))
 
 
 def decode_token(token):
-    return jwt.decode(token, config.SECRET_KEY, algorithms="HS256")
+    try:
+        return jwt.decode(token, config.SECRET_KEY, algorithms="HS256")
+    except Exception as e:
+        abort(401, description=str(e))
 
 
-def jwt_required(optional=False):
+def _get_jwt_identity():
+    if "Authorization" not in request.headers or not request.headers["Authorization"]:
+        return None
+    token = request.headers["Authorization"]
+    decoded_jwt = decode_token(token)
+
+    if not decoded_jwt or not decoded_jwt["identity"]:
+        return None
+    return decoded_jwt["identity"]
+
+
+def jwt_required(required=True):
     def decorator_jwt(f):
         @functools.wraps(f)
         def decorated(*args, **kwargs):
-            identity = get_jwt_identity()
-            if not optional and not identity:
-                return {"message": "Unauthorized."}, 401
+            identity = _get_jwt_identity()
+            if request and not identity:
+                abort(401)
+            # Auto add user_id as kwargs for a method
             kwargs["user_id"] = identity
             return f(*args, **kwargs)
 
         return decorated
 
     return decorator_jwt
-
-
-def get_jwt_identity():
-    token = request.headers["Authorization"]
-    if not token or not decode_token(token):
-        return None
-    else:
-        return decode_token(token)["identity"]
