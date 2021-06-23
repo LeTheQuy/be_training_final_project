23fa256ac8b3162dc7feeb8a874bf8304aa7bfe6
QUY LE THE <quy@gotitapp.co>
2021-06-23 18:51:54 +0700

    fix: minor update

--- main/app.py
+++ main/app.py
@@ -3,32 +3,31 @@ from flask import Flask, jsonify
 from main.config.config import config
 
 app = Flask(__name__)
-app.config[
-    "SQLALCHEMY_DATABASE_URI"] = f"mysql+pymysql://{config.MYSQL_USERNAME}:{config.MYSQL_PASSWORD}" \
-                                 f"@{config.MYSQL_HOST}:{config.MYSQL_PORT}/{config.MYSQL_DB}"
+app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql+pymysql://{config.MYSQL_USERNAME}:{config.MYSQL_PASSWORD}" \
+                                        f"@{config.MYSQL_HOST}:{config.MYSQL_PORT}/{config.MYSQL_DB}"
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.secret_key = config.SECRET_KEY  # todo
+app.secret_key = config.SECRET_KEY
 
 
 @app.errorhandler(404)
 def resource_not_found(e):
-    return jsonify(message=str(e)), 404
+    return {"message": str(e)}, 404
 
 
 @app.errorhandler(500)
 def internal_server_error(e):
-    return jsonify(message=str(e)), 500
+    return {"message": str(e)}, 500
 
 
 @app.errorhandler(401)
 def unauthorized(e):
-    return {"message": "Unauthorized!", }, 401
+    return {"message": "Unauthorized", }, 401
 
 
 @app.errorhandler(403)
 def forbidden(e):
-    return {"message": "Forbidden, permission deny"}, 401
+    return {"message": "Forbidden, permission denied"}, 403
 
 
 def _register_modules():
