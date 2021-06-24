8ff1091522bc5c3435da80fa7969be1397dd192a
QUY LE THE <quy@gotitapp.co>
2021-06-24 11:18:56 +0700

    refactor: separate parse data logi

--- main/helpers/parser.py
+++ main/helpers/parser.py
@@ -7,32 +7,59 @@ from main.models.item import Item
 from main.schemas.parser_schema import ParserSchema
 
 
-def parse_data(check_editable=True):
-    def decorator_parser(f):
+def parse_category():
+    def decorator_parser_category(f):
         @functools.wraps(f)
         def decorated(*args, **kwargs):
             try:
                 data = ParserSchema().dump(kwargs)
             except ValidationError as err:
                 return {"message": err.messages}, 400
-            category, item = None, None
             if "category_id" in data:
                 category = Category.find_by_id(data["category_id"])
                 kwargs["category"] = category
                 if not category:
                     return {"message": "Invalid category id"}, 400
+            return f(*args, **kwargs)
+
+        return decorated
+
+    return decorator_parser_category
+
+
+def parse_item():
+    def decorator_parser_item(f):
+        @functools.wraps(f)
+        def decorated(*args, **kwargs):
+            try:
+                data = ParserSchema().dump(kwargs)
+            except ValidationError as err:
+                return {"message": err.messages}, 400
             if "item_id" in data:
                 item = Item.find_by_id(data["item_id"])
                 kwargs["item"] = item
                 if not item:
                     return {"message": "Invalid item id"}, 400
-            if item and category and item.category_id != category.id:
+            if kwargs["category"] and item.category_id != kwargs["category"].id:
                 return {"message": "Category and item don't match"}, 400
 
-            if check_editable and "user_id" in data and item and item.user_id != data["user_id"]:
-                return {"message": "This item is not editable by yourself"}, 403
             return f(*args, **kwargs)
 
         return decorated
 
-    return decorator_parser
+    return decorator_parser_item
+
+
+def parse_editable_item():
+    def decorator_editable_item(f):
+        @functools.wraps(f)
+        def decorated(*args, **kwargs):
+            editable = False
+            if "user_id" in kwargs and kwargs["item"] and kwargs["item"].user_id == kwargs["user_id"]:
+                editable = True
+            kwargs["editable"] = editable
+            return f(*args, **kwargs)
+
+        return decorated
+
+    return decorator_editable_item
