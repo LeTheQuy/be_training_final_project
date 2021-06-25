5af12afa69f06fbc4385e705c8f1455058796cfe
QUY LE THE <quy@gotitapp.co>
2021-06-25 14:02:49 +0700

    refactor: update by using status_code, data as funtion output

--- test/controllers/auth.py
+++ test/controllers/auth.py
@@ -10,12 +10,10 @@ class TestSignUp(BaseCase):
             "username": "quy_dz_123456",
             "password": "Quy@gotit123"
         }
-        response = self.test_client.post(url="/auth/sign-up", payload=test_user_info)
+        status_code, data = self.test_client.post(url="/auth/sign-up", payload=test_user_info)
 
-        assert response.data
-        assert response.status_code == 201
-
-        data = json.loads(response.data)
+        assert status_code == 201
+        assert data
         assert data["message"] == "User created successfully"
         assert data["user"]["username"] == test_user_info["username"]
         assert type(data["user"]["id"]) is int
@@ -26,24 +24,21 @@ class TestSignUp(BaseCase):
             "password": "Quy@gotit123"
         }
 
-        response = self.test_client.post(url="/auth/sign-up", payload=test_user_info)
-        assert response.status_code == 201
+        status_code, data = self.test_client.post(url="/auth/sign-up", payload=test_user_info)
+        assert status_code == 201
 
-        response = self.test_client.post(url="/auth/sign-up", payload=test_user_info)
+        status_code, data = self.test_client.post(url="/auth/sign-up", payload=test_user_info)
 
-        assert response.status_code == 400
-        assert response.data
-        data = json.loads(response.data)
+        assert status_code == 400
+        assert data
         assert data["message"] == "Duplicated username"
 
     def test_missing_password(self, init_database):
         test_user_info = {
             "username": "quy_dz_123456",
         }
-        response = self.test_client.post(url="/auth/sign-up", payload=test_user_info)
-        assert response.status_code == 400
-        assert response.data
-        data = json.loads(response.data)
+        status_code, data = self.test_client.post(url="/auth/sign-up", payload=test_user_info)
+        assert status_code == 400
         assert data["message"]["password"][0] == "Missing data for required field."
 
 
@@ -55,30 +50,27 @@ class TestSignIn(BaseCase):
             "username": "quy_dz_123456",
             "password": "Quy@gotit123"
         }
-        response = self.test_client.post(url="/auth/sign-up", payload=self.sign_up_user_info)
-        assert response.status_code == 201
+        status_code, data = self.test_client.post(url="/auth/sign-up", payload=self.sign_up_user_info)
+        assert status_code == 201
 
     def test_sign_in_successful(self, init_database):
-        response = self.test_client.post(url="/auth/sign-in", payload=self.sign_up_user_info)
+        status_code, data = self.test_client.post(url="/auth/sign-in", payload=self.sign_up_user_info)
 
-        assert response.status_code == 200
-        data = json.loads(response.data)
+        assert status_code == 200
         assert data["access_token"]
         assert type(data["access_token"]) is str
 
     def test_user_not_found(self, init_database):
         payload = self.sign_up_user_info
         payload["username"] = "quy_dz_1234"
-        response = self.test_client.post(url="/auth/sign-in", payload=payload)
+        status_code, data = self.test_client.post(url="/auth/sign-in", payload=payload)
 
-        assert response.status_code == 401
-        data = json.loads(response.data)
+        assert status_code == 401
         assert data["message"] == "Unauthorized"
 
     def test_empty_password(self, init_database):
         payload = self.sign_up_user_info
         payload["password"] = ""
-        response = self.test_client.post(url="/auth/sign-in", payload=payload)
-        assert response.status_code == 400
-        data = json.loads(response.data)
+        status_code, data = self.test_client.post(url="/auth/sign-in", payload=payload)
+        assert status_code == 400
         assert data["message"]["password"][0] == "Length must be between 8 and 40."
--- test/controllers/categories.py
+++ test/controllers/categories.py
@@ -6,32 +6,28 @@ from test.base_case import BaseCase
 class TestGetCategories(BaseCase):
 
     def test_get_categories_successful(self, init_database):
-        response = self.test_client.get(url="/categories")
-        assert response.status_code == 200
-        data = json.loads(response.data)
+        status_code, data = self.test_client.get(url="/categories")
+        assert status_code == 200
         assert data["categories"]
         assert type(data["categories"]) == list
 
 
 class TestGetCategoryById(BaseCase):
     def test_get_category_by_id_successful(self, init_database):
-        response = self.test_client.get(url="/categories/1")
-        assert response.status_code == 200
-        data = json.loads(response.data)
+        status_code, data = self.test_client.get(url="/categories/1")
+        assert status_code == 200
         assert data["id"]
         assert data["name"]
         assert data["items"]
         assert type(data["items"]) == list
 
     def test_category_not_found(self, init_database):
-        response = self.test_client.get(url="/categories/1000")
-        assert response.status_code == 400
-        data = json.loads(response.data)
+        status_code, data = self.test_client.get(url="/categories/1000")
+        assert status_code == 400
         assert data["message"] == "Invalid category id"
 
     def test_incorrect_category(self, init_database):
-        response = self.test_client.get(url="/categories/abc")
-        assert response.status_code == 404
-        data = json.loads(response.data)
+        status_code, data = self.test_client.get(url="/categories/abc")
+        assert status_code == 404
         assert data["message"] == "404 Not Found: The requested URL was not found on the server. If you entered the " \
                                   "URL manually please check your spelling and try again."
