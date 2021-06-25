541948725cf4f2d2809c7ac6a18a8f4272373d36
QUY LE THE <quy@gotitapp.co>
2021-06-25 09:32:16 +0700

    feat: add dev config

---
+++ main/config/test.py
@@ -0,0 +1,15 @@
+from main.config.base import BaseConfig
+
+
+class TestConfig(BaseConfig):
+    MYSQL_HOST = "127.0.0.1"
+    MYSQL_PORT = 3306
+    MYSQL_USERNAME = "root"
+    MYSQL_PASSWORD = "quyngao123456"
+    MYSQL_DB = "be_training_test"
+    DEBUG = True
+    SECRET_KEY = "be_training_g0tit@^&#quydz__tqn_"
+    ITEMS_PER_PAGE = 10
+
+
+config = TestConfig
