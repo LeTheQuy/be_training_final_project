a5b1b5d3b3c7f8aaee40ad16fe78ab7a1b8ad018
QUY LE THE <quy@gotitapp.co>
2021-06-25 10:29:06 +0700

    fix: move test client to base case

--- test/base_case.py
+++ test/base_case.py
@@ -1,4 +1,20 @@
-from test.conftest import TestClient, client
+import json
+
+from test.conftest import client
+
+
+class TestClient:
+    def __init__(self, flask_client):
+        self.client = client
+        self.headers = {"Content-Type": "application/json"}
+
+    def post(self, url, payload):
+        data = json.dumps(payload)
+        return self.client.post(path=url, headers=self.headers, data=data)
+
+    def get(self, url, payload=None):
+        data = json.dumps(payload)
+        return self.client.get(path=url, headers=self.headers, data=data)
 
 
 class BaseCase:
