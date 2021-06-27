326cf9339eb4b14b70ad70fa1ef645d1f65d9712
QUY LE THE <quy@gotitapp.co>
2021-06-27 14:36:42 +0700

    feat: add find_by_id to db action

--- main/models/db_action.py
+++ main/models/db_action.py
@@ -9,3 +9,7 @@ class BaseAction:
     def delete_on_db(self):
         db.session.delete(self)
         db.session.commit()
+
+    @classmethod
+    def find_by_id(cls, _id):
+        return cls.query.get(_id)
