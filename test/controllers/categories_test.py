82c564ba37f2d1faf8621609709d1ec4982053c0
QUY LE THE <quy@gotitapp.co>
2021-06-27 14:24:05 +0700

    feat: update tcs for categories feature

--- test/controllers/categories_test.py
+++ test/controllers/categories_test.py
@@ -3,7 +3,7 @@ from test.base_case import BaseCase
 
 class TestGetCategories(BaseCase):
 
-    def test_get_categories_successful(self, init_database):
+    def test_get_categories_successful(self):
         status_code, data = self.test_client.get(url="/categories")
         assert status_code == 200
         assert data["categories"]
@@ -11,7 +11,7 @@ class TestGetCategories(BaseCase):
 
 
 class TestGetCategoryById(BaseCase):
-    def test_get_category_by_id_successful(self, init_database):
+    def test_get_category_by_id_successful(self):
         status_code, data = self.test_client.get(url="/categories/1")
         assert status_code == 200
         assert data["id"]
@@ -19,12 +19,12 @@ class TestGetCategoryById(BaseCase):
         assert data["items"]
         assert type(data["items"]) == list
 
-    def test_category_not_found(self, init_database):
+    def test_category_not_found(self):
         status_code, data = self.test_client.get(url="/categories/1000")
         assert status_code == 400
         assert data["message"] == "Invalid category id"
 
-    def test_incorrect_category(self, init_database):
+    def test_incorrect_category(self):
         status_code, data = self.test_client.get(url="/categories/abc")
         assert status_code == 404
         assert data["message"] == "404 Not Found: The requested URL was not found on the server. If you entered the " \
