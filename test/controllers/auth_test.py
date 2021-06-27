7ef73d032343011a0f81ec708418238a2f8ec17a
QUY LE THE <quy@gotitapp.co>
2021-06-27 14:21:53 +0700

    refactor: update tcs for auth controller

--- test/controllers/auth_test.py
+++ test/controllers/auth_test.py
@@ -3,7 +3,7 @@ from test.base_case import BaseCase
 
 class TestSignUp(BaseCase):
 
-    def test_sign_up_successful(self, init_database):
+    def test_sign_up_successful(self):
         test_user_info = {
             "username": "quy_dz_123456",
             "password": "Quy@gotit123"
@@ -16,9 +16,9 @@ class TestSignUp(BaseCase):
         assert data["user"]["username"] == test_user_info["username"]
         assert type(data["user"]["id"]) is int
 
-    def test_duplicated_username(self, init_database):
+    def test_duplicated_username(self):
         test_user_info = {
-            "username": "quy_dz_123456",
+            "username": "quy_dz_1234567",
             "password": "Quy@gotit123"
         }
 
@@ -45,20 +45,18 @@ class TestSignIn(BaseCase):
     def setup(self):
         super().setup()
         self.sign_up_user_info = {
-            "username": "quy_dz_123456",
+            "username": "Quyngao_dz_tqn",
             "password": "Quy@gotit123"
         }
-        status_code, data = self.test_client.post(url="/auth/sign-up", payload=self.sign_up_user_info)
-        assert status_code == 201
 
-    def test_sign_in_successful(self, init_database):
+    def test_sign_in_successful(self):
         status_code, data = self.test_client.post(url="/auth/sign-in", payload=self.sign_up_user_info)
 
         assert status_code == 200
         assert data["access_token"]
         assert type(data["access_token"]) is str
 
-    def test_user_not_found(self, init_database):
+    def test_user_not_found(self):
         payload = self.sign_up_user_info
         payload["username"] = "quy_dz_1234"
         status_code, data = self.test_client.post(url="/auth/sign-in", payload=payload)
@@ -66,7 +64,7 @@ class TestSignIn(BaseCase):
         assert status_code == 401
         assert data["message"] == "Unauthorized"
 
-    def test_empty_password(self, init_database):
+    def test_empty_password(self):
         payload = self.sign_up_user_info
         payload["password"] = ""
         status_code, data = self.test_client.post(url="/auth/sign-in", payload=payload)
